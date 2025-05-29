94620af931e28c08b7aea42f402ff403d4dfb69c
David Lopera <davidloperahenao@gmail.com>
2025-05-29 09:43:26 -0500

    Validación de Cancelo usa el parámetro de Sesion en lugar de EstudianteGrupo.

--- asistenciauco-ms/src/main/java/co/edu/uco/asistenciauco/application/usecase/cancelo/validator/ValidarQueNoCancelo.java
+++ asistenciauco-ms/src/main/java/co/edu/uco/asistenciauco/application/usecase/cancelo/validator/ValidarQueNoCancelo.java
@@ -5,10 +5,11 @@ import co.edu.uco.asistenciauco.application.usecase.validator.ValidationResultVO
 import co.edu.uco.asistenciauco.application.usecase.validator.Validator;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
-public class ValidarQueNoCancelo implements Validator<UUID, ValidationResultVO>{
+public class ValidarQueNoCancelo implements Validator<ArrayList<UUID>, ValidationResultVO>{
 
 	private CanceloRepository canceloRepository;
 	
@@ -21,13 +22,13 @@ public class ValidarQueNoCancelo implements Validator<UUID, ValidationResultVO>{
 
 
 	@Override
-	public ValidationResultVO validate(UUID idEstudianteGrupo) {
+	public ValidationResultVO validate(ArrayList<UUID> data) {
 		
 		var resultadoValidacion = new ValidationResultVO();
 		
-		if(!canceloRepository.existsByEstudianteGrupo_IdAndActivoTrue(idEstudianteGrupo)) {
+		if(!canceloRepository.estudianteCanceloInactivo(data.get(0), data.get(1))) {
 			//TODO: El mensaje debería estar en el catálogo de mensajes.
-			resultadoValidacion.agregarMensaje("El estudiante con identificador " + idEstudianteGrupo + " ha cancelado la materia.");
+			resultadoValidacion.agregarMensaje("El estudiante con identificador " + data.get(0) + " ha cancelado la materia.");
 		}
 		
 		return resultadoValidacion;
